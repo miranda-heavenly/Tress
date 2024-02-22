6fa7193ddd3c86cb2c147e0222f1f6ce4d95695d
Miranda Anya <miranda.anya@meltwater.org>
2024-02-22 22:58:17 +0000

    connected to firebase databse, filter and sort buttons added on shop page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -42,9 +42,69 @@ class _HomePageState extends State<HomePage> {
             icon: Icon(
               Icons.menu,
               color: Colors.black,)),
-        )
+        ),
+        title: Text("Tress Hair Store"),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: Icon(Icons.logout),
+          )
+          ],
       ),
-      drawer: Drawer(),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          mainAxisAlignment:MainAxisAlignment.spaceBetween,
+          children: [    
+             Column(
+               children: [
+                //logo
+                 DrawerHeader(
+                  child: Image.asset("images/logo crop.png",
+                             color: Colors.white,
+                             width: 150,)),
+                             
+                             //drawer elements
+                            Padding(
+                 padding: const EdgeInsets.only(left: 35, top: 20, bottom: 20),
+                 child: ListTile(
+                  leading: Icon(Icons.home_outlined, color: Colors.white,),
+                  title: Text("Home",
+                  style: TextStyle(color: Colors.white,),),
+                 ),
+                            ),
+                             Padding(
+                 padding: const EdgeInsets.only(left: 35,  bottom: 20),
+                 child: ListTile(
+                  leading: Icon(Icons.info_outline, color: Colors.white,),
+                  title: Text("About",
+                  style: TextStyle(color: Colors.white,),),
+                 ),
+                            ),
+                 Padding(
+                 padding: const EdgeInsets.only(left: 35,  bottom: 20),
+                 child: ListTile(
+                  leading: Icon(Icons.help_center_outlined, color: Colors.white,),
+                  title: Text("Help",
+                  style: TextStyle(color: Colors.white,),),
+                 ),
+                            ),
+               ],
+             ),
+             Padding(
+                 padding: const EdgeInsets.only(left: 35,  bottom: 100),
+                 child: ListTile(
+                  leading: Icon(Icons.logout_outlined, color: Colors.white,),
+                  title: Text("Log out",
+                  style: TextStyle(color: Colors.white,),),
+                 ),
+                            ),
+           
+          ],
+        ),
+      ),
+
       bottomNavigationBar: MyBottomNavBar(
         onTabChange: (index) => navigateBottomNavBar(index),
       ),
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:tress/shared_widgets/dropdown_menu.dart';
 
 class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
@@ -10,8 +13,86 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Shop Page")
+    return Column(
+      children: [
+       
+           
+        //Search bar
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Search", style: TextStyle(
+                    color: Colors.grey,
+                  ),),
+                  Icon(Icons.search, color: Colors.grey,)
+                ],
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 8,),
+
+        //Message
+        // Padding(
+        //   padding: const EdgeInsets.symmetric(vertical: 20),
+        //   child: Text("Find your perfect tress, embrace your confidence.",
+        //   style: TextStyle(color: Colors.grey[700]),),
+        // ),
+
+         //Sort
+          Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 14),
+          child: Row(
+            children: [
+             Flexible(
+               child: DropDownMenu(
+                defaultText: "Sort", 
+                items: ["Low to high", "High to low"], 
+                onSelected: (selected){
+                }),
+             ),
+              Flexible(
+               child: DropDownMenu(
+                defaultText: "Sort", 
+                items: ["Low to high", "High to low"], 
+                onSelected: (selected){
+                }),
+             )
+            ],
+          ),
+        ),
+
+
+
+
+
+        //Categories
+         Container(
+          height: 50,
+          padding: EdgeInsets.symmetric(horizontal: 14),
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 5,
+            itemBuilder:(context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(5),
+                child: Chip(label: Text("Category")),
+              );
+            },),
+        ),
+
+        
+      ],
+
     );
-  }
-}
+}}
